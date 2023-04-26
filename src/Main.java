04dc4e3d8905e67542cb3d9c31cea58fb93637f4
Ruslan Binyatov <binyatovruslan85@gmail.com>
2023-04-26 13:59:13 +0300

    My initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+// Написать программу для вывода «Hello World» на консоль
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
