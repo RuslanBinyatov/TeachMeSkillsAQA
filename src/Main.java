02785461feef450b78441aab39170339ba055475
Ruslan Binyatov <binyatovruslan85@gmail.com>
2023-05-12 21:24:21 +0300

    Homework_5

--- src/Main.java
+++ src/Main.java
@@ -12,10 +12,11 @@ public class Main {
          */
     public static void getArrayEntryNum() {
         int[] numbers = new int[] {1, 2, 3, 4, 5};
+        System.out.println(numbers[0]);
 
-        Scanner num = new Scanner(System.in);
-        System.out.print("Please enter any integer: ");
-        int num = num.nextInt();
+        Scanner integer = new Scanner(System.in);
+        System.out.print("Please, enter any integer: ");
+        int num = integer.nextInt();
 
 
     }
