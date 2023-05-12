d579db4deff0c1d8d158a3169c03bf3e78f17038
Ruslan Binyatov <binyatovruslan85@gmail.com>
2023-05-12 22:23:03 +0300

    Homework_5

--- src/Main.java
+++ src/Main.java
@@ -11,20 +11,19 @@ public class Main {
         Пусть число для поиска задается с консоли (класс Scanner).
          */
     public static void getArrayEntryNum() {
-        int[] numbers = new int[] {1, 2, 3, 4, 5};
+        int[] numbers = new int[]{1, 2, 3, 4, 5};
 
         Scanner integer = new Scanner(System.in);
         System.out.print("Please, enter any integer: ");
         int num = integer.nextInt();
 
-        for (int index = 0; index < numbers.length; index++) {
+        for (int index = 0; index < numbers.length; index++)
             //System.out.print(numbers[index] + " ");
             if (num == index) {
             }
-            System.out.print(numbers[index] + " ");
-                //System.out.println(num + " ");
-            }
-            else if {
+            //System.out.print(numbers[index] + " ");
+            //System.out.println(num + " ");
+            else {
                 System.out.println("The entered value is not a positive number, please try again");
             }
 
