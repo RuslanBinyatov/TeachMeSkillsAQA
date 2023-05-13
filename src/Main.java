78ef492e837cbe96b27bab63dbd9d73d1484fa75
Ruslan Binyatov <binyatovruslan85@gmail.com>
2023-05-13 19:22:25 +0300

    Homework_5

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 // Homework_5
 import java.util.Scanner;
 import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+
         getArrayEntryNum();
+        //getArrayWithoutNum();
     }
     // Одномерные массивы
         /*
@@ -19,17 +22,27 @@ public class Main {
         int num = integer.nextInt();
 
         int key = Arrays.binarySearch(numbers, num);
-        System.out.print(numbers[key] + " ");
 
-        //for (int index = 0; index < numbers.length; index++) {
-        // System.out.print(numbers[index] + " ");
-        //if (num == index) {
-        //System.out.println(num + " ");
-        //}
-        //System.out.print(numbers[index] + " ");
-        // else {
-        // System.out.println("The entered value is not a positive number, please try again");
-        //}
-        //}
+        if () {
+            System.out.print(numbers[key] + " ");
+        } else {
+            System.out.println("The entered value is not a positive number, please try again");
+        }
     }
+        /*
+        1. Создайте массив целых чисел. Удалите все вхождения заданного числа из массива.
+        Пусть число задается с консоли (класс Scanner). Если такого числа нет - выведите сообщения об этом.
+        В результате должен быть новый массив без указанного числа.
+         */
+        public static void getArrayWithoutNum() {
+            int[] notnum = new int[]{1, 2, 3, 4, 5};
+
+            Scanner integer = new Scanner(System.in);
+            System.out.print("Please, enter any integer: ");
+            int num = integer.nextInt();
+
+            int key = Arrays.binarySearch(numbers, num);
+
+
+        }
 }
