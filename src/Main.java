a825b2cc16025c27b337ec5242fe585d37798d86
Ruslan Binyatov <binyatovruslan85@gmail.com>
2023-05-16 21:37:44 +0300

    Homework_6

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main
 {
     public static void main(String[] args) {
-        getThreeArrayEachElementNum();
+        getThreeDimArrayEachElementNum();
         getChessBoard();
     }
     public static Scanner scanner() {
@@ -17,18 +17,18 @@ public class Main
         С помощью циклов "пройти" по всему массиву и увеличить каждый элемент на заданное число.
         Пусть число, на которое будет увеличиваться каждый элемент, задается из консоли.
         */
-    public static void getThreeArrayEachElementNum() {
-        int [][][] threeArray = new int [][][] {
+    public static void getThreeDimArrayEachElementNum() {
+        int [][][] threeDimArray = new int [][][] {
                 {
                         {1, 2, 3},
                         {4, 5, 6},
                         {7, 8, 9}
                 }
         };
-        for (int dm1 = 0; dm1 < threeArray.length; dm1++) {
-            for (int dm2 = 0; dm2 < threeArray[dm1].length; dm2++) {
-                for (int dm3 = 0; dm3 < threeArray[dm1][dm2].length; dm3++) {
-                    System.out.print(threeArray[dm1][dm2][dm3] + "  ");
+        for (int dm1 = 0; dm1 < threeDimArray.length; dm1++) {
+            for (int dm2 = 0; dm2 < threeDimArray[dm1].length; dm2++) {
+                for (int dm3 = 0; dm3 < threeDimArray[dm1][dm2].length; dm3++) {
+                    System.out.print(threeDimArray[dm1][dm2][dm3] + "  ");
                 }
                 System.out.println();
             }
@@ -51,12 +51,12 @@ public class Main
         B W B W B W B W
         */
     public static void getChessBoard() {
-        String [][] doubleArray = new String [8][8];
-        for (int i = 0; i < doubleArray.length; i++) {
-            for (int j = 0; j < doubleArray[0].length; j++) {
-                if ((i + j) % 2 == 0) doubleArray [i][j] = "W";
-                else doubleArray [i][j] = "B";
-                System.out.print(doubleArray[i][j] + "  ");
+        String [][] twoDimArray = new String [8][8];
+        for (int i = 0; i < twoDimArray.length; i++) {
+            for (int j = 0; j < twoDimArray[i].length; j++) {
+                if ((i + j) % 2 == 0) twoDimArray [i][j] = "W";
+                else twoDimArray [i][j] = "B";
+                System.out.print(twoDimArray[i][j] + "  ");
             }
             System.out.println();
         }
