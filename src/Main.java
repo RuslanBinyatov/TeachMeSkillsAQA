4849c64d4dc5ac69d65102bd112e3b75abcd21bc
Ruslan Binyatov <binyatovruslan85@gmail.com>
2023-05-21 13:45:00 +0300

    Homework_7

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-// Написать программу для вывода «Hello World» на консоль
-public class Main
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World");
-    }
-}
